f8ce98079ac243f89cf4002de93e28a9502505a3
zh0rchik <georgiy.zyuzin73@mail.ru>
2024-07-03 23:34:37 +0400

    ShopSerializer теперь имеет два метода: отдельный для создания и отдельный для отображения

--- shops/serializers.py
+++ shops/serializers.py
@@ -20,11 +20,19 @@ class CityStreetsSerializer(serializers.ModelSerializer):
         model = City
         fields = ('city_name', 'streets')
 
-
 class ShopSerializer(serializers.ModelSerializer):
-    city = CitySerializer(read_only=True)
-    street = StreetSerializer(read_only=True)
+    city_name = serializers.CharField(source='city.city_name', read_only=True)
+    street_name = serializers.CharField(source='street.street_name', read_only=True)
 
     class Meta:
         model = Shop
-        fields = ('shop_name', 'street', 'city', 'home', 'open_time', 'close_time')
+        fields = ('shop_name', 'city_name', 'street_name', 'city', 'street', 'home', 'open_time', 'close_time')
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['city'] = representation.pop('city_name')
+        representation['street'] = representation.pop('street_name')
+        return representation
+
+    def create(self, validated_data):
+        return Shop.objects.create(**validated_data)
